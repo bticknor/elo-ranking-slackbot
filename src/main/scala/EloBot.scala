ffdf11aaaf44c0aa1815baaa2f1e6e20f3b911c0
bticknor <bticknor@truefit.com>
2019-02-10 13:40:06 -0500

    parametrize routine for what to do on message

--- src/main/scala/EloBot.scala
+++ src/main/scala/EloBot.scala
@@ -1,4 +1,5 @@
 import slack.rtm.SlackRtmClient
+import slack.models.Message
 import akka.actor.ActorSystem
 import slack.SlackUtil
 import com.redis._
@@ -9,21 +10,26 @@ object PingPongBot extends App {
   implicit val system = ActorSystem("slack")
   implicit val ec = system.dispatcher
 
+  // Connection token
   val token = "xoxb-151175863378-ckhDjYJDrGDM3Wpu40ijKwLp"
   
   // Connection to local redis server
   val redisClient = new RedisClient("localhost", 6379)
+
   // RTM connection to Slack
   val slackClient = SlackRtmClient(token)
 
+  // ID of bot
   val selfId = slackClient.state.self.id
 
-  slackClient.onMessage { message =>
-  val mentionedIds = SlackUtil.extractMentionedIds(message.text)
-
-	if(mentionedIds.contains(selfId)) {
-	  slackClient.sendMessage(message.channel, s"<@${message.user}>: Sup dawg!")
-	}
+  // Action to take 
+  def onMessageAction(message: Message): Unit = {
+    val mentionedIds = SlackUtil.extractMentionedIds(message.text)
+    if(mentionedIds.contains(selfId)) {
+      slackClient.sendMessage(message.channel, s"<@${message.user}>: Sup dawg!")
+    }
   }
+
+  slackClient.onMessage(onMessageAction)
 }
 
