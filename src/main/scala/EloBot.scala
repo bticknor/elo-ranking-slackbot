ec48b319728b97de3cac31a813f1e436c527457a
bticknor <bticknor@truefit.com>
2019-02-10 13:16:34 -0500

    add connection to redis

--- src/main/scala/EloBot.scala
+++ src/main/scala/EloBot.scala
@@ -1,6 +1,7 @@
 import slack.rtm.SlackRtmClient
 import akka.actor.ActorSystem
 import slack.SlackUtil
+import com.redis._
 
 
 object PingPongBot extends App {
@@ -8,9 +9,12 @@ object PingPongBot extends App {
   implicit val system = ActorSystem("slack")
   implicit val ec = system.dispatcher
 
-
   val token = "xoxb-151175863378-ckhDjYJDrGDM3Wpu40ijKwLp"
-  val client = SlackRtmClient(token)
+  
+  // Connection to local redis server
+  val redisClient = new RedisClient("localhost", 6379)
+  // RTM connection to Slack
+  val slackClient = SlackRtmClient(token)
 
   val selfId = client.state.self.id
 
