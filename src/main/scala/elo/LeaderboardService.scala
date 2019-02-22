502bcc16f0223d14b0b431ad8b689f75ad7a2fca
Tim Wong <twongkk25@gmail.com>
2019-02-22 13:21:40 -0500

    fixed formatLeaderboard to properly build the leaderboard message string

--- src/main/scala/elo/LeaderboardService.scala
+++ src/main/scala/elo/LeaderboardService.scala
@@ -10,16 +10,10 @@ class LeaderboardService(playerService: PlayerService) {
     if (topPlayers.isEmpty) {
       "No scores yet!"
     } else {
-      val formattedTopPlayers = topPlayers
-        .zipWithIndex
-        .map { case (player, rank) =>
-          s"$rank. <@${player.slackUserId}> ${player.formattedScore}"
-        }
-      s"""
-      True Fit top ${topPlayers.size} Ping Pongers:
-
-      $formattedTopPlayers
-      """
+      topPlayers
+        .zip(1 to topPlayers.size)
+        .map { case (player, rank) => s"$rank. <@${player.slackUserId}> ${player.formattedScore}" }
+        .foldLeft(s"True Fit top ${topPlayers.size}\n")((concatMsg, playerLine) => concatMsg + "\n" + playerLine)
     }
   }
 }
