4b4971d56688079a45bc625ee67701fb721ee3e3
Benjamin Ticknor <ticknorbenjamin@gmail.com>
2019-03-02 12:17:30 -0500

    add method for parsing the size of the leaderboard requested by the user
    the logic is as follows:
    - look for the position of the "leaderboard" token in the message
    - find the token immdiately following it, try to cast it to int

--- src/main/scala/elo/LeaderboardService.scala
+++ src/main/scala/elo/LeaderboardService.scala
@@ -1,6 +1,21 @@
 package elo
+import scala.util.Try
 
 class LeaderboardService(playerService: PlayerService) {
+
+  def parseSizeOfLeaderboard(messageText: String): Option[Int] = {
+    // get position of token after first "leaderboard" invocation
+    val splitCommand = messageText.split(" ")
+    val posNumPlayers = splitCommand
+      .map(x => x.contains("eaderboard"))
+      .indexOf(true) + 1
+    // "leaderboard" is last token in message, return None
+    // if the token after "leaderboard" cannot be cast to int, return None
+    if(posNumPlayers == splitCommand.length) None else{
+      val numPlayersShow = splitCommand(posNumPlayers)
+      Try(numPlayersShow.toInt).toOption
+    }
+  }
 
   def getLeaderboard(numTopPlayers: Int = 3): Seq[Player] = {
     playerService.findAllPlayers.sortBy(-_.score).take(numTopPlayers)
